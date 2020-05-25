5db65859a9f0ad149c4100feaaa3b944091d7130
alex <alex-v-pg@hotmail.fr>
2020-05-25 20:20:11 +0200

    Add new page "Habitations"

--- ANGclient/src/app/pages/liste-appart/liste-appart.component.ts
+++ ANGclient/src/app/pages/liste-appart/liste-appart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-liste-appart',
   templateUrl: './liste-appart.component.html',
@@ -9,48 +9,45 @@ export class ListeAppartComponent implements OnInit  {
 
   private url2 = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr_crous_logement_france_entiere&q=&facet=zone&refine.zone=Paris+18";
 
-
+  public appart: appartement[];
+  response: any;
   constructor(private http : HttpClient) {
+
    }
 
-  ngOnInit() {
+  ngOnInit(){
+
+
     let test = [];
-    let appart: appartement = {nom : null, Adresse:null,Arrondissement : null ,
-      photo : null ,
-      telephone : null,
-      Accés : null,
-      records : null
-    }
-    this.http.get<coordonnées>(this.url2).subscribe(
-      (response) =>{
+    this.http.get(this.url2).subscribe(result => {
+
 
 
         //console.log(response.records[1]);
         //this.array_coordonnes = response.map( Objet => new coo )
         //console.log(this.array_coordonnes);
 
-
-        for (let index = 0; index < response.records.length; index++) {
+        //@ts-ignore
+        for (let index = 0; index < result.records.length; index++) {
           //@ts-ignore
           //console.log(response.records.length);
-          test[index] = response.records[index].geometry.coordinates[index];
+
           //console.log(response.records[index]);
           //@ts-ignore
-          let appart_nom = response.records[index].fields.title;
+          let appart_nom = result.records[index].fields.title;
+          //console.log(appart_nom);
           //@ts-ignore
-          let appart_adress = response.records[index].fields.address;
+          let appart_adress = result.records[index].fields.address;
+          //console.log(appart_adress);
           //@ts-ignore
-          let appart_image = response.records[index].fields.photo;
-          appart.nom = appart_nom;
-          appart.Adresse = appart_adress;
-          appart.photo = appart_image;
-          console.log(appart.nom);
-          console.log(appart.Adresse);
-          console.log(appart.photo);
+          let appart_image = result.records[index].fields.photo;
 
-          //console.log(response.records[index].geometry.coordinates);
-          //console.log(response.records[index].geometry.coordinates);
-          //console.log(response.records[index].geometry.coordinates[1]);
+          this.appart = result['records']
+          //console.log(this.appart);
+
+
+
+
 
 
         }
@@ -67,14 +64,12 @@ export class ListeAppartComponent implements OnInit  {
 
 
 
-  }
+
   }
 
-  interface coordonnées{
-    records: coordonnées[];
-    geometry: coordonnées[];
-    coordinates : coordonnées[];
   }
+
+
 
   interface appartement {
 
@@ -84,6 +79,6 @@ export class ListeAppartComponent implements OnInit  {
     photo : string ,
     telephone : string,
     Accés : string,
-    records : appartement[]
+    fields : any,
 
   }
