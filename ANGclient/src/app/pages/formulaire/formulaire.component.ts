8a52047920078f38239603bd2aaed65fdf753706
alex <alex-v-pg@hotmail.fr>
2020-05-27 16:59:58 +0200

    update formulaire

--- ANGclient/src/app/pages/formulaire/formulaire.component.ts
+++ ANGclient/src/app/pages/formulaire/formulaire.component.ts
@@ -80,17 +80,15 @@ createEmployee(data): Observable<any> {
 
     console.log('Enregistrement terminé ! ');
     })
-    console.log(this.loginForm.value.email);
+
     var json_client = JSON.stringify(this.loginForm.value);
     this.router.navigateByUrl('/validation');
 
 
-    this.http.post('https://api.elasticemail.com/v2/email/send?apikey=B6D96F9A2CD2EFB3ED89B328032A92607BC3D409E3AB44A0738FF769E2BD3AAE38A4E6FDCA8B983DB8F7A93A0493EA07&subject=Validation de votre demande &from=alex-v-pg@hotmail.fr&fromName=&sender=&senderName=&msgFrom=&msgFromName=&replyTo=&replyToName=&to='+this.loginForm.value.email+'&msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&dataSource=&channel=&bodyHtml=Bonjour <br> Merci pour votre demande , vous serez prochainement mis en contact par l équipe <br><br> Cordialement <br> L équipe Coeur de Logement&bodyText=&charset=&charsetBodyHtml=&charsetBodyText=&encodingType=0&template=&headers_firstname=firstname: myValueHere&postBack=&merge_firstname=John&timeOffSetMinutes=&poolName=My Custom Pool&isTransactional=false&attachments=&trackOpens=true&trackClicks=true&utmSource=source1&utmMedium=medium1&utmCampaign=campaign1&utmContent=content1&bodyAmp=&charsetBodyAmp=',this.loginForm.value, ).subscribe(()=>{
+    this.http.post('https://api.elasticemail.com/v2/email/send?apikey=B6D96F9A2CD2EFB3ED89B328032A92607BC3D409E3AB44A0738FF769E2BD3AAE38A4E6FDCA8B983DB8F7A93A0493EA07&subject=Validation de votre demande &from=alex-v-pg@hotmail.fr&fromName=&sender=&senderName=&msgFrom=&msgFromName=&replyTo=&replyToName=&to='+this.loginForm.value.email+'&msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&dataSource=&channel=&bodyHtml=Bonjour '+this.loginForm.value.prenom+' <br> Merci pour votre demande , vous serez prochainement mis en contact par l équipe <br><br> Cordialement <br> L équipe Coeur de Logement&bodyText=&charset=&charsetBodyHtml=&charsetBodyText=&encodingType=0&template=&headers_firstname=firstname: myValueHere&postBack=&merge_firstname=John&timeOffSetMinutes=&poolName=My Custom Pool&isTransactional=false&attachments=&trackOpens=true&trackClicks=true&utmSource=source1&utmMedium=medium1&utmCampaign=campaign1&utmContent=content1&bodyAmp=&charsetBodyAmp=',this.loginForm.value, ).subscribe(()=>{
 
-      console.log('Enregistrement terminé ! ');
       })
 
-    console.log("email envoyé");
 
 
   }
